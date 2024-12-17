f1bb625bab660f9f445d099f6158f9408b82f414
Andres Goyes <andres.goyes@correounivalle.edu.co>
2024-12-17 13:53:11 -0500

    Updates v1.8

--- router/index.js
+++ router/index.js
@@ -6,6 +6,22 @@ import routerAuth from './auth.js'
 
 const router = Router();
 
+router.get('/', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Mytineraries</title>
+        </head>
+        <body>
+            <h1>Mytineraries - Backend</h1>
+        </body>
+        </html>
+    `);
+});
+
 router.use('/cities', citiesRouter);
 router.use('/itineraries', itinerariesRouter);
 router.use('/users', userRouter);
