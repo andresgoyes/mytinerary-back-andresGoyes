67b90f505f9b4d0a1b809313b9c0e46c31809c37
Andres Goyes <andres.goyes@correounivalle.edu.co>
2024-12-17 09:49:20 -0500

    Updates v1.1

--- router/auth.js
+++ router/auth.js
@@ -9,13 +9,13 @@ import passport from "../middlewares/passport.js";
 import passportGoogle from "../middlewares/passportGoogle.js";
 
 const routerAuth = Router()
-routerAuth.post('/signIn', accountNotExist, isValidPassword, generateToken, signIn)
-routerAuth.post('/signOut', passport.authenticate('jwt', { session: false }), signOut)
+routerAuth.post('/signin', accountNotExist, isValidPassword, generateToken, signIn)
+routerAuth.post('/signout', passport.authenticate('jwt', { session: false }), signOut)
 
-routerAuth.get('/signIn/google',
+routerAuth.get('/signin/google',
     passportGoogle.authenticate('google', { session: false, scope: ['profile', 'email'] }))
     
-routerAuth.get('/signIn/google/callback',
+routerAuth.get('/signin/google/callback',
     passportGoogle.authenticate('google', { session: false, failureRedirect: '/login' }), 
     generateToken,
     signInGoogle)
