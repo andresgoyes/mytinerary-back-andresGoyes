e25caec08696249bd63d90826ba70d6109d5c6ec
Andres Goyes <andres.goyes@correounivalle.edu.co>
2024-12-17 13:18:52 -0500

    Updates v1.5

--- middlewares/createHash.js
+++ middlewares/createHash.js
@@ -1,11 +1,13 @@
 import bcryptjs from "bcryptjs";
 
 export default async (req, res, next) => {
-    let password = req.body.password
-    let hashPassword = bcryptjs.hashSync(
-        password,
-        10
-    )
-    req.body.password = hashPassword
-    return next()
+    try {
+        let password = req.body.password;
+        let hashPassword = await bcryptjs.hash(password, 10);
+
+        req.body.password = hashPassword;
+        return next();
+    } catch (error) {
+        next(error);
+    }
 }
--- middlewares/generateToken.js
+++ middlewares/generateToken.js
@@ -1,11 +1,24 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
+  try {
+    const { user, body } = req;
+    const { id, email: userEmail, role, profile } = user || {};
+    const { userId: bodyUserId, email: bodyEmail } = body || {};
+    const profileEmail = profile?.emails?.[0]?.value;
+
+    const email = userEmail || bodyEmail || profileEmail;
+    const userId = id || bodyUserId || profileEmail;
+
     const token = jwt.sign(
-        { email: req.body.email || req.user.email },
-        process.env.SECRET,
-        { expiresIn: 60 * 5 }
-    )
-    req.token = token
-    return next()
-}
+      { email, role, userId },
+      process.env.SECRET,
+      { expiresIn: 60 * 60 * 2 }
+    );
+
+    req.token = token;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
